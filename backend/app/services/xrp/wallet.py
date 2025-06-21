0d72057c60dbf1b311ad8f66b8ff5f50022686d4
Frederic <frederic.taieb@gmail.com>
2025-06-21 12:22:42 +0000

    Orgnisation du code et ajout de fonctionnalités

--- backend/app/services/xrp/wallet.py
+++ backend/app/services/xrp/wallet.py
@@ -1,28 +1,28 @@
-from xrpl.clients import JsonRpcClient
+# app/services/xrp/wallet.py
+
+from xrpl.asyncio.wallet import generate_faucet_wallet
+from xrpl.asyncio.clients import AsyncJsonRpcClient
 from xrpl.models.requests import AccountInfo
 from xrpl.utils import drops_to_xrp 
 from app.utils.logger import logger_init
 import logging
+import os
 
-
-logger_init()
+logger_init(level=logging.INFO)
 logger = logging.getLogger(__name__) 
 
+JSON_RPC_URL = os.getenv("XRP_RPC_URL", "https://s.altnet.rippletest.net:51234/")
 
-
-def create_wallet():
-    from xrpl.wallet import generate_faucet_wallet
-    JSON_RPC_URL = "https://s.altnet.rippletest.net:51234/"
-    client = JsonRpcClient(JSON_RPC_URL)
-    wallet = generate_faucet_wallet(client)
+async def create_wallet():
+    client = AsyncJsonRpcClient(JSON_RPC_URL)
+    wallet = await generate_faucet_wallet(client)
     logger.info(f"Wallet created: {wallet}")
     return wallet
 
-def get_xrp_balance(wallet_address):
-    from xrpl.clients import JsonRpcClient
-    JSON_RPC_URL = "https://s.altnet.rippletest.net:51234/"
-    client = JsonRpcClient(JSON_RPC_URL)
+async def get_xrp_balance(wallet_address):
+    client = AsyncJsonRpcClient(JSON_RPC_URL)
     req = AccountInfo(account=wallet_address, ledger_index="validated", strict=True)
-    response = client.request(req)
-    logger.info(f"Balance: {response.result['account_data']['Balance']}")
-    return drops_to_xrp(response.result['account_data']['Balance'])
+    response = await client.request(req)
+    balance_drops = response.result["account_data"]["Balance"]
+    logger.info(f"Balance: {balance_drops} drops")
+    return drops_to_xrp(balance_drops)
